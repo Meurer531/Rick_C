abbaf16c224ea41cb7ee9f947a4d6ef4e027424d
Meurer531 <148451701+Meurer531@users.noreply.github.com>
2024-11-11 22:15:35 -0300

    Update soma.h

--- soma.h
+++ soma.h
@@ -28,7 +28,7 @@ void sub() {
 void divi() {
     int num1, num2;
     printf("Digite o primeiro numero: \n");
-    scanf("%d", &num1);
+    scanf("%d", &num1);a
     printf("Digite o segundo numero: \n");
     scanf("%d", &num2);
     if (num2 != 0) {
@@ -74,6 +74,63 @@ void fibo()
     }
 }
 
+void sorteio() {
+    int soma = 0;
+    int numero_sorteado;
+    int contador = 0;
+
+    printf("Iniciando o sorteio... 1-10, até 21 \n");
+
+    while (soma <= 21) {
+
+        numero_sorteado = (contador % 10) + 1;
+        contador++;
+
+
+        soma += numero_sorteado;
+
+
+        printf("Numero sorteado: %d\n", numero_sorteado);
+        printf("Soma atual: %d\n", soma);
+
+
+        if (soma > 21) {
+            printf("A soma total ultrapassou 21. Voce perdeu!\n");
+            return;
+        }
+
+
+        char continuar;
+        printf("Deseja continuar? (s/n): ");
+        scanf(" %c", &continuar);
+
+        if (continuar == 'n' || continuar == 'N') {
+            printf("Voce escolheu parar com a soma total de %d. Fim do jogo.\n", soma);
+            return;
+        }
+    }
+}
+
+void sorteios()
+{
+    const char* mensagens[] = {
+        "Você é mais forte do que pensa!",
+        "Nunca desista dos seus sonhos!",
+        "Acredite em si mesmo, o impossível é só uma questão de perspectiva.",
+        "O sucesso é a soma de pequenos esforços repetidos dia após dia."
+    };
+
+    // Mensagem permanente
+    const char* mensagemPermanente = "A vinganca nunca eh plena, mata a alma e envenena!";
+
+    // Gerar número aleatório para escolher uma das 4 mensagens
+    srand(time(NULL)); // Inicializa a semente do gerador de números aleatórios
+    int indice = rand() % 4;  // Gera um número entre 0 e 3
+
+    
+    printf("Mensagem sorteada: %s\n", mensagens[indice]);
+    printf("Mensagem permanente: %s\n", mensagemPermanente);
+}
 
 
 #endif
